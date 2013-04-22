9ad1a443b45eb823766ca4d74e43ca379e9dd165
Rudy Castro <rudmansays@yahoo.com>
2013-04-22 19:07:40 -0400

    update 20130422

--- Chats/src/edu/ncc/chats/SMSReceiver.java
+++ Chats/src/edu/ncc/chats/SMSReceiver.java
@@ -7,14 +7,20 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
+
+//I think/know the problem is that this is not getting executed
+//not sure why be because the manifest is set up to listen for 
+//this receiver.
 public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(Context arg0, Intent arg1) {
 		//get message passed in
-		Bundle bundle = intent.getExtras();
+		Bundle bundle = arg1.getExtras();
 		SmsMessage[] messages = null;
 		String str = "";
+		
+		
 		if(bundle != null)
 		{
 			Object[] pdus = (Object[]) bundle.get("pdus");
@@ -28,7 +34,13 @@ public class SMSReceiver extends BroadcastReceiver {
 				str += "\n";
 			}
 			//display the message
-			Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+			Toast.makeText(arg0, str, Toast.LENGTH_LONG).show();
+			
+			//send a broad cast Intent to update the SMS receiver in the in ongoing/and main
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction("SMS_RECEIVED_ACTION");
+            broadcastIntent.putExtra("sms",str);
+            arg0.sendBroadcast(broadcastIntent);
 		}
 
 	}
